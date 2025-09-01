d72c9dc4604ded9bf3977cc76f9de2b3c1bf6091
AbuBakkarSiddique007 <abubakkar.cce25.iiuc@gmail.com>
2025-09-01 16:29:57 +0600

    Update: Connect firebase auth

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),]
+    tailwindcss(),
+  ],
+  define: {
+    global: 'globalThis',
+  },
+  resolve: {
+    alias: {
+      events: 'events',
+    }
+  },
+  optimizeDeps: {
+    include: [
+      'firebase/app',
+      'firebase/auth',
+      'firebase/firestore',
+      'events'
+    ]
+  }
 })
