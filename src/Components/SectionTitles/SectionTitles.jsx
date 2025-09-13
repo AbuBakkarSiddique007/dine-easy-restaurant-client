4628f5a15871d2b5576f4daafa1893a9ad22775b
AbuBakkarSiddique007 <abubakkar.cce25.iiuc@gmail.com>
2025-09-13 17:09:54 +0600

    style: modify the sectionTitle component

--- src/Components/SectionTitles/SectionTitles.jsx
+++ src/Components/SectionTitles/SectionTitles.jsx
@@ -1,15 +1,12 @@
-import React from 'react';
-
 const SectionTitles = ({ heading, subHeading }) => {
     return (
-        <div className='max-w-lg text-center mx-auto'>
-            <p className=' text-xl text-yellow-500 italic pb-4 pt-10'>
-                ---{subHeading} ---
+        <div className='max-w-lg text-center mx-auto mb-8'>
+            <p className='text-lg text-yellow-500 italic mb-3'>
+                ---{subHeading}---
             </p>
-            <h2 className='text-4xl py-5 border-t-2 border-t-gray-400 border-b-2 border-b-gray-400 mb-12' >
+            <h2 className='text-3xl md:text-4xl font-bold py-3 border-t-2 border-t-gray-400 border-b-2 border-b-gray-400 text-gray-800'>
                 {heading}
             </h2>
-
         </div>
     );
 };
